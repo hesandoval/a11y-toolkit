27e08c042b3685e97227293dcebc15ed8cefa5fa
Edgar Sandoval Walle <hsandovalwalle@apple.com>
2017-08-22 13:21:21 -0700

    Added timeout to insure message is read. :

--- src/announce.js
+++ src/announce.js
@@ -4,7 +4,7 @@ function announce(message, manner) {
   var announcer, clear_announcer;
    manner = manner || 'polite';
    announcer = document.getElementById('a11y-toolkit-announcer');
-    
+
    if (announcer === null) {
      console.warn('Page does not implement an element with id `a11y-toolkit-announcer`. This message will not be announced.');
      return
@@ -15,9 +15,12 @@ function announce(message, manner) {
      announcer.innerHTML = '';
      announce_timeout = null;
    };
-   announcer.setAttribute('aria-live', manner);
-   announcer.innerHTML = message;
+   set_announcer = function() {
+     announcer.innerHTML = message
+     announcer.setAttribute('aria-live', manner);
+   }
    clearTimeout(announce_timeout);
+   announce_timeout = setTimeout(set_announcer, 50);
    announce_timeout = setTimeout(clear_announcer, 500);
 }
 
